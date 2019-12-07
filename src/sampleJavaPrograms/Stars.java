bf49173bc2df73a89e1fc10ed378e8a1785e4c3b
eswar2993 <58638628+eswar2993@users.noreply.github.com>
2019-12-08 02:04:46 +0530

    Create Stars.java
    Included sample Java programs package to work on java  program techniques

---
+++ src/sampleJavaPrograms/Stars.java
@@ -0,0 +1,25 @@
+package sampleJavaPrograms;
+
+public class Stars {
+
+	public static void main(String[] args) {
+		
+		stars(5);
+	}
+	
+	public static void stars(int count){
+		for(int i=1;i<=count;i++){
+			for(int j=0;j<i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}	
+	}
+	
+	
+	
+	
+	
+	
+
+}
