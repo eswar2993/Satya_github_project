bd2aa0151e9ff8e2fbdff64ed04b3798cd84353b
eswar2993 <58638628+eswar2993@users.noreply.github.com>
2019-12-08 01:37:12 +0530

    Create addition.java

---
+++ src/Project1/addition.java
@@ -0,0 +1,13 @@
+package Project1;
+
+public class addition {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Sum of 3 and 2 is: "+addition(3,2));
+	}
+	public static double addition(double a, double b){
+		
+		return a+b;
+	}
+}
